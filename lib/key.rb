eb7e8ca24bd5b47aa81885e0ee01cd18f2e1825f
Kelly Sandeson <kellysandeson@gmail.com>
2019-06-01 17:19:08 -0600

    Change key_grouping method to be error free

--- lib/key.rb
+++ lib/key.rb
@@ -9,19 +9,22 @@ class Key
     @c = []
     @d = []
     @random_keys = []
+    @key_group = []
   end
 
-  def random_numbers
+  def random_num
     5.times { @random_keys << rand(0..9)}
     @random_keys
   end
 
   def key_grouping
-    a = @random_keys.combination(2).to_a
-      b = a.uniq(&:first)
-        c = b.map{ |arr| arr.join }
-          d = c.map{ |num| num.to_i }
-          d
+    random_num.each_cons(2){ |num| @key_group << num }
+    @key_group.map { |arr| arr.join.to_i }
+    @key_group
+  end
+
+  def key_assignment
+  
   end
 
 end
