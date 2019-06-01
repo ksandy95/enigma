cfcce6afeefdf51997ac6c5f6dac0d571b568871
Kelly Sandeson <kellysandeson@gmail.com>
2019-06-01 13:40:08 -0600

    Create test for key assignment to variable groups

--- test/key_test.rb
+++ test/key_test.rb
@@ -30,4 +30,11 @@ class KeyTest < Minitest::Test
     assert_equal [], @key_1.key_grouping
   end
 
+  def test_key_assignment
+    assert_equal [], @key_1.a
+    assert_equal [], @key_1.b
+    assert_equal [], @key_1.c
+    assert_equal [], @key_1.d
+  end
+
 end
