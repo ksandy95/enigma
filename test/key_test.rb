21446b467327aaf4b3849afb28f8d264cc12bc78
Kelly Sandeson <kellysandeson@gmail.com>
2019-06-01 13:32:39 -0600

    Create test for instance variables

--- test/key_test.rb
+++ test/key_test.rb
@@ -15,4 +15,11 @@ class KeyTest < Minitest::Test
     assert_instance_of Key, @key_1
   end
 
+  def test_it_has_arrays
+    assert_equal [], @key_1.a
+    assert_equal [], @key_1.b
+    assert_equal [], @key_1.c
+    assert_equal [], @key_1.d
+  end
+
 end
