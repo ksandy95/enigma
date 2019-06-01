1a2c5ec3220af65aab6a5cdd5944dc8d9933d953
Kelly Sandeson <kellysandeson@gmail.com>
2019-06-01 11:30:59 -0600

    Creation of enigma method skeletons

---
+++ lib/enigma.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Enigma
+
+
+  def initialize(argument)
+    @argument = argument
+  end
+
+  def encrypt
+    # The encrypt method takes a message String as an argument.
+    # It can optionally take a Key and Date as arguments to use for encryption.
+    # If the key is not included, generate a random key.
+    # If the date is not included, use today’s date.
+  end
+
+  def decrypt
+    # The decrypt method takes a ciphertext String and the Key used
+    # for encryption as arguments. The decrypt method can optionally take a date
+    # as the third argument. If no date is given,
+    # this method should use today’s date for decryption.
+  end
+
+end
