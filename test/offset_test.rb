6febf265ccb7c45d90aa2bfdd8001c13b12c63db
Kelly Sandeson <kellysandeson@gmail.com>
2019-06-01 17:31:08 -0600

    Create test names

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -6,6 +6,20 @@ require 'date'
 
 class OffsetTest < Minitest::Test
 
-  
+  def test_it_exists
+
+  end
+
+  def test_it_has_date
+
+  end
+
+  def test_date_numeric_square
+
+  end
+
+  def test_digits_of_squared_date
+
+  end
 
 end
