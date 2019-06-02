196414574800b67871f781c53df4af79cdaa5226
Kelly Sandeson <kellysandeson@gmail.com>
2019-06-02 16:52:00 -0600

    Create offset arguments

--- lib/offset.rb
+++ lib/offset.rb
@@ -2,16 +2,13 @@ require 'pry'
 require './lib/key.rb'
 class Offset
 
-  def initialize
-    @key = Key.new
-  end
-
-  def date
-    Time.now.strftime("%m%d%y")
+  def initialize(date = date_1)
+    @date = @date_1 unless date
+    @date_1 = Time.now.strftime("%m%d%y")
   end
 
   def numeric_date
-    date.to_i
+    @date_1.to_i
   end
 
   def square_date
@@ -21,13 +18,6 @@ class Offset
   def last_four
     a = square_date.to_s.slice(-4..-1).split(//)
     a.map{ |x| x.to_i }
-  end
-
-  def offset_total
-    a = []
-    a << @key.key_grouping
-    a << last_four
-    a.transpose.map {|x| x.reduce(:+)}
   end
 
 end
