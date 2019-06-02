2fe5b19fa6b7efd1d699a2db6e23a00ab7edf713
Kelly Sandeson <kellysandeson@gmail.com>
2019-06-01 20:51:56 -0600

    Create character split test

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -18,10 +18,17 @@ class ShiftTest < Minitest::Test
   end
 
   def test_abcd_shifts
-    assert_equal 0, @shift.a
-    assert_equal 1, @shift.b
-    assert_equal 3, @shift.c
-    assert_equal 4, @shift.d
+    assert_equal 0, @shift.a.keys
+    assert_equal 1, @shift.b.keys
+    assert_equal 3, @shift.c.keys
+    assert_equal 4, @shift.d.keys
+  end
+
+  def test_character_split
+    assert_equal [], @shift.a.values
+    assert_equal [], @shift.b.values
+    assert_equal [], @shift.c.values
+    assert_equal [], @shift.d.values
   end
 
 end
