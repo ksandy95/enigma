9d51172de4e19998c5d4c1aee45b1816e8c82a5e
Kelly Sandeson <kellysandeson@gmail.com>
2019-06-02 17:46:40 -0600

    Refactored for correct argument name

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,14 +1,13 @@
 require 'pry'
-require './lib/key.rb'
-class Offset
 
-  def initialize(date = date_1)
-    @date = @date_1 unless date
-    @date_1 = Time.now.strftime("%m%d%y")
+class Offset
+  attr_reader :date
+  def initialize(date = Time.now.strftime("%d%m%y"))
+    @date = date
   end
 
   def numeric_date
-    @date_1.to_i
+    @date.to_i
   end
 
   def square_date
@@ -16,8 +15,8 @@ class Offset
   end
 
   def last_four
-    a = square_date.to_s.slice(-4..-1).split(//)
-    a.map{ |x| x.to_i }
+    last_digits = square_date.to_s.slice(-4..-1).split(//)
+    last_digits.map{ |digit| digit.to_i }
   end
 
 end
