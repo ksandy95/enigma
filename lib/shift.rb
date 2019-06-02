e3c343cbc36b0b57a1a437404324ad2de4ea85d7
Kelly Sandeson <kellysandeson@gmail.com>
2019-06-02 00:02:50 -0600

    Create hash for offset keys and values

--- lib/shift.rb
+++ lib/shift.rb
@@ -8,4 +8,12 @@ class Shift
     @offset = Offset.new
   end
 
+  def letter_divide
+    @letters[:a] = @offset.offset_total[0]
+    @letters[:b] = @offset.offset_total[1]
+    @letters[:c] = @offset.offset_total[2]
+    @letters[:d] = @offset.offset_total[3]
+    @letters
+  end
+
 end
