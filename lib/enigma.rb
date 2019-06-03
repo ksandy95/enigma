7c33dfeee74034874ad2fa08584795dcafc4e693
Kelly Sandeson <kellysandeson@gmail.com>
2019-06-03 13:33:34 -0600

    Create decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,14 +3,13 @@ class Enigma
 
   def encrypt(message, key = Key.new.key, date = Offset.new.date)
     shift = Shift.new(key, date)
-    encrypted = {
-      encryption: shift.character_shift(message),
+    { encryption: shift.character_shift(message),
       key: key, date: date}
   end
 
-  # def decrypt(key = Key.new.key, date = Offset.new.date)
-  #   {decryption: encrypted[:encryption],
-  #   key: key, date: date }
-  # end
+  def decrypt(message, key, date = Offset.new.date)
+    shift = Shift.new(key, date)
+    {decryption: shift.reverse_shift(message), key: key, date: date }
+  end
 
 end
