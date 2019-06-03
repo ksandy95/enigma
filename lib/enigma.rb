08fa9bf4aa4d80b849283c599c4fdf77ce9de3c9
Kelly Sandeson <kellysandeson@gmail.com>
2019-06-02 21:40:49 -0600

    edit encrypt method defaults

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,12 +4,14 @@ require './lib/key'
 
 class Enigma
 
-  def encrypt(message, key = Key.new, date = Offset.new )
-
+  def encrypt(message, key = Key.new.key, date = Offset.new.date)
+    shift = Shift.new(key, date)
+    {encryption: shift.character_shift(message),
+      key: key, date: date}
   end
 
-  def decrypt(encrypted[:encryption], key, date = Offset.new)
-
-  end
+  # def decrypt(encrypted, key, date = Offset.new)
+  #
+  # end
 
 end
