f6e42a9a2e77110c9129f6b98c9efebd8b23942e
Kelly Sandeson <kellysandeson@gmail.com>
2019-06-02 19:38:41 -0600

    Update character shift rotations

--- lib/shift.rb
+++ lib/shift.rb
@@ -23,17 +23,12 @@ class Shift
     shift
   end
 
-  def character_shift
-    message.downcase.each_char { |character|
-    @range.find(charater)}
+  def character_shift(message)
+    message.downcase.chars.map do |character|
+      next character if !@range.include?(character)
+      index = @range.find_index(character)
+      @range.rotate(index + shift_rotation).first
+    end.join
   end
-  # message.each_char { |x| conditional for abcd offsets }
 
-  def character_loop
-    #at the start of the string, for every character, downcase.
-    # the characters start out at the beginning of the range and when shifted
-    # the index position is summed with the offset total to get the new index position.
-    # at the beginning to the string, the first character is shifted by the
-    # value of @letters[:a] to correspond to the range.
-  end
 end
